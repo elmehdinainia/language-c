484be08bb38d9e476d3a591b923e07cfc813a68e
elmehdinainia <mehdinainiaa4@gmail.com>
2021-11-12 00:19:32 +0100

    erregistrement

--- chal11.c
+++ chal11.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-	    int rect,langeur,largeur;
-	    printf("entrer la valeur de langeur : ");
-	    scanf("%d",&langeur);
-		printf("entrer la valeur de largeur : ");
-		scanf("%d",&largeur);
+	    int a b,c;
+	    printf("entrer 3 valeur \n :");
+	    scanf("%d",&a);
+	    a=c;
+	    b=(a%10)*100;
+	    a=a/10;
+	    b-b(a%10)*10;
+	    a\
+
+		
+
+		
 		rect = 2*(langeur + largeur);
 		printf("la valeur de rect = %d",rect);
 		     }
