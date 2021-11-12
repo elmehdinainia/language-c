ce30c3b47f6566f5afe4dfdfcf2330f639c655c8
elmehdinainia <mehdinainiaa4@gmail.com>
2021-11-12 17:03:16 +0100

    ajouter

--- les conditions/challenfe2.c
+++ les conditions/challenfe2.c
@@ -4,25 +4,26 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(){
-	int nbr,rest;
+	int nbr;
 	printf("donner une nombre : ");
 	scanf("%d",&nbr);
+	 
+	 if ( nbr==0)
+	 {
+	 	printf("le nombre est null");
+	 }
+	 
 	
 
-	if (nbr%2==0)
+    else if (nbr%2==0)
 	{
 		printf("le nombre %d est paire",nbr);
 	}
-	else
+	else 
     {
     	printf("le nombre est inpaire");
 	}
     
-	
-
-    
-	
-	
 	return 0;
 }
 
