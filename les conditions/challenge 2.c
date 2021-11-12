a013b990aa782043b7f44c335f239094d715cd4b
elmehdinainia <mehdinainiaa4@gmail.com>
2021-11-12 14:45:20 +0100

    challenge

---
+++ les conditions/challenge 2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(){
+	int nbr,rest;
+	printf("donner une nombre : ");
+	scanf("%d",&nbr);
+	
+
+	if (nbr%2==0)
+	{
+		printf("le nombre %d est paire",nbr);
+	}
+	else
+    {
+    	printf("le nombre est inpaire");
+	}
+    
+	
+
+    
+	
+	
+	return 0;
+}
