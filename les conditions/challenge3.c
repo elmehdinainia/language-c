778d3a215fd420c94b529f3ab27872e2bc5a75c5
elmehdinainia <mehdinainiaa4@gmail.com>
2021-11-12 15:38:26 +0100

    ajouter nn

---
+++ les conditions/challenge3.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	char car;
+	printf("donner un cararctaire : ");
+	scanf("%c",&car);
+	switch (car)
+	{   
+	 case 'e':
+	 	{
+	 		printf("ce caractaire sest voyelle : "); 
+			 break;	
+		 }
+
+    case'u':
+    	{
+		printf("ce caractaire est voyelle");
+		break;
+		}
+    	
+    case 'a':
+    	{
+    	printf("ce caractaire est voyelle");
+    	break;
+	
+		}
+	case 'i':
+	    {
+	  	printf("ce caractaire est voyelle");
+	  	break;
+		}
+
+	case 'y':
+    	{
+    	printf("ce caractaire est voyelle");
+    	break;
+    	}
+    	default:
+    		{
+    			printf(" non");
+    			break;
+			}
+			
+	   	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
