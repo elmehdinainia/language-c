8b197898598b79002b7ecf9e0d5503f68b36b62f
elmehdinainia <mehdinainiaa4@gmail.com>
2021-11-12 09:46:19 +0100

    novelle enregisstrement

---
+++ chal8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	    int rect,langeur,largeur;
+	    printf("entrer la valeur de langeur : ");
+	    scanf("%d",&langeur);
+		printf("entrer la valeur de largeur : ");
+		scanf("%d",&largeur);
+		rect = 2*(langeur + largeur);
+		printf("la valeur de rect = %d",rect);
+		     }
---
+++ chal9.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	float circonference , r , pi = 3.14;
+	printf("entrer la valeur de rayon : ");
+	scanf("%f",&r);
+	circonference = r * pi *2;
+	printf("circonference = %f",circonference );
+	
+
+}
+
+
+
+	
+	
+
+	
+	
+	
+	
+	
+	
+
